3802e3367f7981a7a13b6554648f94cc8ea1ba07
王侨文 <wangqiaowen@itcast.cn>
2019-10-19 15:36:36 +0800

    Ok

--- day04/04-源代码/yougou/src/pages/order/index.vue
+++ day04/04-源代码/yougou/src/pages/order/index.vue
@@ -5,13 +5,36 @@
         <li :class="{active:index==activeIndex}"
             v-for="(item, index) in orderMenu"
             :key="index"
-            @click="activeIndex=index">{{item.name}}</li>
+            @click="activeIndex=index">{{item}}</li>
       </ul>
     </div>
 
     <div class="content">
       <ul>
-        <li>
+        <li v-for="(item, index) in orderList"
+            :key="item.order_id">
+          <div class="goods-info"
+               v-for="(item2, index2) in item.goods"
+               :key="item2.goods_id">
+            <img :src="item2.goods_small_logo"
+                 alt="">
+            <div class="right">
+              <p>{{item2.goods_name}}</p>
+              <div class="price-num">
+                <span class="price">￥{{item2.goods_price}}</span>
+                <span class="num">x{{item2.goods_number}}</span>
+              </div>
+            </div>
+          </div>
+          <p class="total-price">
+            共{{item.total_count}}件商品 总计：&yen;{{item.total_price}}(含运费0.00)
+          </p>
+          <div class="order">
+            <span>订单号:{{item.order_number}}</span>
+            <button type="primary">
+              支付
+            </button>
+          </div>
 
         </li>
       </ul>
@@ -26,24 +49,13 @@ export default {
   data () {
     return {
       orderMenu: [
-        {
-          name: '全部',
-          id: 'allOrder'
-        },
-        {
-          name: '待付款',
-          id: 'prePay'
-        },
-        {
-          name: '待收货',
-          id: 'payed'
-        },
-        {
-          name: '退款/退货',
-          id: 'refund'
-        }
+        '全部',
+        '待付款',
+        '待收货',
+        '退款/退货'
       ],
-      activeIndex: 0
+      activeIndex: 0,
+      orderList: []
     }
   },
   onLoad (options) {
@@ -57,7 +69,7 @@ export default {
         wx.navigateTo({ url: '/pages/login/main' })
       }
       request({
-        url: `/api/public/v1/my/orders/all?type=2`,
+        url: `/api/public/v1/my/orders/all?type=1`,
         method: 'GET',
         header: {
           'Authorization': token
@@ -66,6 +78,7 @@ export default {
         let { meta, message } = res.data
         console.log(message)
         if (meta.status === 200) {
+          this.orderList = message.orders
         }
       })
     }
@@ -86,8 +99,8 @@ export default {
     li {
       flex: 1;
       text-align: center;
-      height: 102rpx;
-      line-height: 102rpx;
+      height: 100rpx;
+      line-height: 100rpx;
       box-sizing: border-box;
       &.active {
         border-bottom: 12rpx solid #eb4450;
@@ -97,7 +110,62 @@ export default {
 }
 .content {
   li {
+    background-color: #fff;
     margin-top: 30rpx;
+    padding: 0 20rpx;
+    .goods-info {
+      height: 260rpx;
+      border-bottom: 1rpx solid #ddd;
+      display: flex;
+      align-items: center;
+
+      img {
+        width: 200rpx;
+        height: 200rpx;
+        margin-right: 26rpx;
+        flex-shrink: 0;
+      }
+      .right {
+        p {
+          overflow: hidden;
+          text-overflow: ellipsis;
+          display: -webkit-box;
+          -webkit-line-clamp: 2; // 控制多行的行数
+          -webkit-box-orient: vertical;
+        }
+        .price-num {
+          margin-top: 84rpx;
+          display: flex;
+          justify-content: space-between;
+          .price {
+            color: #eb4450;
+          }
+        }
+      }
+    }
+    .total-price {
+      height: 90rpx;
+      line-height: 90rpx;
+      border-bottom: 1rpx solid #ddd;
+      text-align: right;
+      color: #999;
+    }
+    .order {
+      height: 88rpx;
+      color: #999;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      span {
+        flex: 1;
+        font-size: 14px;
+      }
+      button {
+        width: 160rpx;
+        height: 60rpx;
+        line-height: 60rpx;
+      }
+    }
   }
 }
 </style>
